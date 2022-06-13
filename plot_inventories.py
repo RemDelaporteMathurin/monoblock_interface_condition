b4b92cb1e3067f9eb9dc956d3e965df90b8b0e28
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-06-13 08:01:31 +0000

    several exposures on one plot

--- plot_inventories.py
+++ plot_inventories.py
@@ -1,30 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data_concentration = np.genfromtxt(
-    "results/concentration_continuity/derived_quantities.csv", delimiter=",", names=True
-)
+phi_heat = 7e6  # W/m2
+phi_imp = 1e21  # H/m2/s
 
-t_concentration = data_concentration["ts"]
+for phi_heat, phi_imp in zip([3e6, 6e6, 7e6], [1e21, 1e21, 1e21]):
 
-inventory_concentration = sum(
-    [data_concentration["Total_retention_volume_{}".format(vol_id)] for vol_id in [8]]
-)
+    folder = "results/phi_heat={:.1e}_phi_imp={:.1e}".format(phi_heat, phi_imp)
 
-data_chemical = np.genfromtxt(
-    "results/chemical_potential_continuity/derived_quantities.csv",
-    delimiter=",",
-    names=True,
-)
+    data_concentration = np.genfromtxt(
+        folder + "/concentration_continuity/derived_quantities.csv",
+        delimiter=",",
+        names=True,
+    )
 
-t_chemical = data_chemical["ts"]
+    t_concentration = data_concentration["ts"]
 
-inventory_chemical = sum(
-    [data_chemical["Total_retention_volume_{}".format(vol_id)] for vol_id in [8]]
-)
+    inventory_concentration = sum(
+        [
+            data_concentration["Total_retention_volume_{}".format(vol_id)]
+            for vol_id in [8]
+        ]
+    )
 
-plt.plot(t_concentration, inventory_concentration)
-plt.plot(t_chemical, inventory_chemical)
+    data_chemical = np.genfromtxt(
+        folder + "/chemical_potential_continuity/derived_quantities.csv",
+        delimiter=",",
+        names=True,
+    )
+
+    t_chemical = data_chemical["ts"]
+
+    inventory_chemical = sum(
+        [data_chemical["Total_retention_volume_{}".format(vol_id)] for vol_id in [8]]
+    )
+
+    plt.plot(t_concentration, inventory_concentration)
+    plt.plot(t_chemical, inventory_chemical)
 plt.yscale("log")
 plt.xscale("log")
 plt.show()
